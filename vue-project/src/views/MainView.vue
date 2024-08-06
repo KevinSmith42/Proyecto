6eea58fb611b758732996ec97a13852e22dabc5a
Lizardo <lacertiliaseason@gmail.com>
2024-08-06 08:41:50 -0500

    Creé componente lista de tareas, borrar componentes autogenerados, crear rutas

---
+++ vue-project/src/views/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+    <Sidebar></Sidebar>
+    <div class="content">
+        <Router-view></Router-view>
+    </div>
+</div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import Sidebar from '../components/Sidebar.vue';
+</script>
+
+<style>
+.container {
+    display: flex;
+}
+
+.content {
+    flex: 1;
+    padding: 20px;
+}
+</style>
